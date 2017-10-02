161597bbcad2dd5fd6a80abf4377817c3962b372
Graham Earley <graham.p.earley@gmail.com>
2017-10-02 17:13:43 +0530

    delete lesson headers if lesson is deleted

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,13 @@ exports.addLessonHeader = functions.database.ref('{languageCode}/subtopic_lesson
 		// Grab the current value of what was written to the Realtime Database.
 		const lesson = event.data.val();
 		const lessonKey = event.data.key;
+
+		const headerPath = event.params.languageCode + "/subtopic_lesson_headers/" + event.params.topicId + "/" + event.params.subtopicId + "/" + event.params.lessonId;
+		const headerRef = admin.database().ref(headerPath);
+
+		if (!lesson) {
+			return headerRef.remove();
+		}
 		
 		const authorEmail = lesson["authorEmail"];
 		const authorInstitution = lesson["authorInstitution"];
@@ -35,8 +42,6 @@ exports.addLessonHeader = functions.database.ref('{languageCode}/subtopic_lesson
 			"topic": event.params.subtopicId
 		}
 
-		const headerPath = event.params.languageCode + "/subtopic_lesson_headers/" + event.params.topicId + "/" + event.params.subtopicId + "/" + lessonKey;
-		const headerRef = admin.database().ref(headerPath);
 		return headerRef.set(lessonHeader);
     });
 
@@ -46,6 +51,13 @@ exports.updateFeaturedLessonHeader = functions.database.ref('{languageCode}/subt
 		const lessonHeader = event.data.val();
 		const headerKey = event.data.key;
 
+		const featuredHeaderPath = event.params.languageCode + "/featured_subtopic_lesson_headers/" + event.params.topicId + "/" + event.params.subtopicId;
+		const featuredHeaderRef = admin.database().ref(featuredHeaderPath);
+
+		if (!lessonHeader) {
+			return featuredHeaderRef.remove();
+		}
+
 		const subtopicSubmissionPath = event.params.languageCode + "/subtopic_lessons/" + event.params.topicId + "/" + event.params.subtopicId;
 		const subtopicSubmissionRef = admin.database().ref(subtopicSubmissionPath);
 
@@ -54,18 +66,12 @@ exports.updateFeaturedLessonHeader = functions.database.ref('{languageCode}/subt
 			lessonHeader["subtopicSubmissionCount"] = submissionCount;
 			
 			if (lessonHeader["isFeatured"]) {
-				const featuredHeaderPath = event.params.languageCode + "/featured_subtopic_lesson_headers/" + event.params.topicId + "/" + event.params.subtopicId;
-				const featuredHeaderRef = admin.database().ref(featuredHeaderPath);
 				return featuredHeaderRef.set(lessonHeader);
 			} else {
-				const featuredHeaderSubtopicPath = event.params.languageCode + "/featured_subtopic_lesson_headers/" + event.params.topicId + "/" + event.params.subtopicId;
-				const featuredHeaderSubtopicRef = admin.database().ref(featuredHeaderSubtopicPath);
-
-				return featuredHeaderSubtopicRef.once('value').then(function(dataSnapshot) {
+				return featuredHeaderRef.once('value').then(function(dataSnapshot) {
 					if (dataSnapshot.hasChildren()) {
 						// ==> this featured header has items in it ==> it exists!
-						const submissionCount = dataSnapshot.numChildren();
-						const subtopicSubmissionRef = featuredHeaderSubtopicRef.child("subtopicSubmissionCount");
+						const subtopicSubmissionRef = featuredHeaderRef.child("subtopicSubmissionCount");
 						return subtopicSubmissionRef.set(submissionCount);
 					} else {
 						// There is no featured header to update.
